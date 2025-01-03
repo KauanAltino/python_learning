158196ab6292ad203345ff49689d6801be2f67c9
K.Altino <kauanaltino@gmail.com>
2025-01-03 12:19:47 -0300

    Ex4 Resolvido (ajuste no codigo)

--- Exercicio4/IMC.py
+++ Exercicio4/IMC.py
@@ -17,11 +17,11 @@ try:
         print("Você está abaixo do peso")
     elif 18.5 <= imc < 25:
         print("Você está no peso normal")
-    elif 25 >= imc <= 29:
+    elif 25 <= imc < 30:
         print("Você está acima do peso")
-    elif 30 >= imc <= 34.99:
+    elif 30 <= imc < 35:
         print("Você está com obesidade grau 1")
-    elif 35 >= imc <= 39.99:
+    elif 35 <= imc < 40:
         print("Você está com obesidade grau 2")
     elif imc >= 40:
         print("Você está com obesidade grau 3")
